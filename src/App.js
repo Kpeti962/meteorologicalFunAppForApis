aa2abb1788db290912323830eee9ed04bc5002a6
Peti <kpeti962@gmailcom>
2022-08-25 12:54:28 +0200

    fun with modals

--- src/App.js
+++ src/App.js
@@ -15,6 +15,20 @@ function App() {
   const [weather8, setWeather8] = useState("");
   const [weather9, setWeather9] = useState("");
   const [weather10, setWeather10] = useState("");
+  
+  const [modal, setModal] = useState(false);
+  const [modal1, setModal1] = useState(false);
+  const [modal2, setModal2] = useState(false);
+  const [modal3, setModal3] = useState(false);
+  const [modal4, setModal4] = useState(false);
+  const [modal5, setModal5] = useState(false);
+  const [modal6, setModal6] = useState(false);
+  const [modal7, setModal7] = useState(false);
+  const [modal8, setModal8] = useState(false);
+  const [modal9, setModal9] = useState(false);
+  const [modal10, setModal10] = useState(false);
+
+  
 
   useEffect(() => {
     axios
@@ -23,10 +37,7 @@ function App() {
       )
       .then((data) => {
         setWeather(data.data);
-
         console.log(data.data);
-        console.log(data.data.current.temp_c);
-        console.log(data.data.current.condition.icon);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -131,89 +142,208 @@ function App() {
       .catch((err) => console.log(err));
   }, []);
 
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+  const toggleModal1 = () => {
+    setModal1(!modal1);
+  };
+  const toggleModal2 = () => {
+    setModal2(!modal2);
+  };
+  const toggleModal3 = () => {
+    setModal3(!modal3);
+  };
+  const toggleModal4 = () => {
+    setModal4(!modal4);
+  };
+  const toggleModal5 = () => {
+    setModal5(!modal5);
+  };
+  const toggleModal6 = () => {
+    setModal6(!modal6);
+  };
+  const toggleModal7 = () => {
+    setModal7(!modal7);
+  };
+  const toggleModal8 = () => {
+    setModal8(!modal8);
+  };
+  const toggleModal9 = () => {
+    setModal9(!modal9);
+  };
+  const toggleModal10 = () => {
+    setModal10(!modal10);
+  };
+  
   return (
     <div className="countryWeatherWrapper">
-      {weather &&
-        weather1 &&
-        weather2 &&
-        weather3 &&
-        weather4 &&
-        weather5 &&
-        weather6 &&
-        weather7 &&
-        weather8 &&
-        weather9 &&
-        weather10 && (
-          <div className="countryImg">
-            <img className="country" src={hungary} alt="" />
-            <div className="szeged">
-              <h5>{weather.location.name}</h5>
-              <h5>{weather.current.temp_c}°C</h5>
+      <div className="countryImg">
+        <img className="country" src={hungary} alt="" />
+        {weather && (
+          <div className="szeged">
+            <h3 onClick={toggleModal}>{weather.location.name}</h3>
+            <div onClick={toggleModal} className="overlay"></div>
+            {modal &&(<div className="openModal">
+            <h3 onClick={toggleModal}>{weather.location.name}</h3>
+              <h3>{weather.current.temp_c}°C</h3>
               <img src={weather.current.condition.icon} alt="" />
-              <h6>{weather.current.condition.text}</h6>
-            </div>
-            <div className="pecs">
-              <h5>{weather1.location.name}</h5>
-              <h5>{weather1.current.temp_c}°C</h5>
-              <img src={weather1.current.condition.icon} alt="" />
-              <h6>{weather1.current.condition.text}</h6>
-            </div>
-            <div className="keszthely">
-              <h5>{weather2.location.name}</h5>
-              <h5>{weather2.current.temp_c}°C</h5>
-              <img src={weather2.current.condition.icon} alt="" />
-              <h6>{weather2.current.condition.text}</h6>
-            </div>
-            <div className="szombathely">
-              <h5>{weather3.location.name}</h5>
-              <h5>{weather3.current.temp_c}°C</h5>
-              <img src={weather3.current.condition.icon} alt="" />
-              <h6>{weather3.current.condition.text}</h6>
-            </div>
-            <div className="sopron">
-              <h5>{weather4.location.name}</h5>
-              <h5>{weather4.current.temp_c}°C</h5>
-              <img src={weather4.current.condition.icon} alt="" />
-              <h6>{weather4.current.condition.text}</h6>
-            </div>
-            <div className="gyor">
-              <h5>{weather5.location.name}</h5>
-              <h5>{weather5.current.temp_c}°C</h5>
-              <img src={weather5.current.condition.icon} alt="" />
-              <h6>{weather5.current.condition.text}</h6>
-            </div>
-            <div className="szekesfehervar">
-              <h5>{weather6.location.name}</h5>
-              <h5>{weather6.current.temp_c}°C</h5>
-              <img src={weather6.current.condition.icon} alt="" />
-              <h6>{weather6.current.condition.text}</h6>
-            </div>
-            <div className="budapest">
-              <h5>{weather7.location.name}</h5>
-              <h5>{weather7.current.temp_c}°C</h5>
-              <img src={weather7.current.condition.icon} alt="" />
-              <h6>{weather7.current.condition.text}</h6>
-            </div>
-            <div className="szolnok">
-              <h5>{weather8.location.name}</h5>
-              <h5>{weather8.current.temp_c}°C</h5>
-              <img src={weather8.current.condition.icon} alt="" />
-              <h6>{weather8.current.condition.text}</h6>
-            </div>
-            <div className="miskolc">
-              <h5>{weather9.location.name}</h5>
-              <h5>{weather9.current.temp_c}°C</h5>
-              <img src={weather9.current.condition.icon} alt="" />
-              <h6>{weather9.current.condition.text}</h6>
-            </div>
-            <div className="debrecen">
-              <h5>{weather10.location.name}</h5>
-              <h5>{weather10.current.temp_c}°C</h5>
-              <img src={weather10.current.condition.icon} alt="" />
-              <h6>{weather10.current.condition.text}</h6>
-            </div>
+              <h4>Szél erősség: {weather.current.wind_kph} km/h</h4>
+              <h4>Hőérzet:{weather.current.feelslike_c} °C</h4>
+              <h4>Páratartalom: {weather.current.humidity} %</h4>
+
+              <button className="closeModal" onClick={toggleModal}>Bezár</button>
+            </div>)}
           </div>
         )}
+        {weather1 && (
+          <div className="pecs">
+            <h3  onClick={toggleModal1}>{weather1.location.name}</h3>
+            <div onClick={toggleModal1} className="overlay"></div>
+            {modal1 &&(<div className="openModal">
+            <h3  onClick={toggleModal1}>{weather1.location.name}</h3>
+            <h3>{weather1.current.temp_c}°C</h3>
+            <img src={weather1.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather1.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather1.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather1.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal1}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather2 && (
+          <div className="keszthely">
+            <h3   onClick={toggleModal2}>{weather2.location.name}</h3>
+            <div onClick={toggleModal2} className="overlay"></div>
+            {modal2 &&(<div className="openModal">
+            <h3   onClick={toggleModal2}>{weather2.location.name}</h3>
+            <h3>{weather2.current.temp_c}°C</h3>
+            <img src={weather2.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather2.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather2.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather2.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal2}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather3 && (
+          <div className="szombathely">
+            <h3 onClick={toggleModal3}>{weather3.location.name}</h3>
+            <div onClick={toggleModal3} className="overlay"></div>
+            {modal3 &&(<div className="openModal">
+            <h3   onClick={toggleModal3}>{weather3.location.name}</h3>
+            <h3>{weather3.current.temp_c}°C</h3>
+            <img src={weather3.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather3.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather3.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather3.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal3}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather4 && (
+          <div className="sopron">
+            <h3 onClick={toggleModal4}>{weather4.location.name}</h3>
+            <div onClick={toggleModal1} className="overlay"></div>
+            {modal4 &&(<div className="openModal">
+            <h3 onClick={toggleModal4}>{weather4.location.name}</h3>
+            <h3>{weather4.current.temp_c}°C</h3>
+            <img src={weather4.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather4.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather4.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather4.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal4}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather5 && (
+          <div className="gyor">
+            <h3 onClick={toggleModal5}>{weather5.location.name}</h3>
+            <div onClick={toggleModal5} className="overlay"></div>
+            {modal5 &&(<div className="openModal">
+            <h3 onClick={toggleModal5}>{weather5.location.name}</h3>
+            <h3>{weather5.current.temp_c}°C</h3>
+            <img src={weather5.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather5.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather5.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather5.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal5}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather6 && (
+          <div className="szekesfehervar">
+            <h3 onClick={toggleModal6}>{weather6.location.name}</h3>
+            <div onClick={toggleModal6} className="overlay"></div>
+            {modal6 &&(<div className="openModal">
+            <h3 onClick={toggleModal6}>{weather6.location.name}</h3>
+            <h3>{weather6.current.temp_c}°C</h3>
+            <img src={weather6.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather6.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather6.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather6.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal6}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather7 && (
+          <div className="budapest">
+            <h3 onClick={toggleModal7}>{weather7.location.name}</h3>
+            <div onClick={toggleModal7} className="overlay"></div>
+            {modal7 &&(<div className="openModal">
+            <h3 onClick={toggleModal7}>{weather7.location.name}</h3>
+            <h3>{weather7.current.temp_c}°C</h3>
+            <img src={weather7.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather7.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather7.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather7.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal7}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather8 && (
+          <div className="szolnok">
+            <h3 onClick={toggleModal8}>{weather8.location.name}</h3>
+            <div onClick={toggleModal8} className="overlay"></div>
+            {modal8 &&(<div className="openModal">
+            <h3>{weather8.current.temp_c}°C</h3>
+            <img src={weather8.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather8.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather8.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather8.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal8}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather9 && (
+          <div className="miskolc">
+            <h3 onClick={toggleModal9}>{weather9.location.name}</h3>
+            <div onClick={toggleModal9} className="overlay"></div>
+            {modal9 &&(<div className="openModal">
+            <h3>{weather9.current.temp_c}°C</h3>
+            <img src={weather9.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather9.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather9.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather9.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal9}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+        {weather10 && (
+          <div className="debrecen">
+            <h3 onClick={toggleModal10}>{weather10.location.name}</h3>
+            <div onClick={toggleModal10} className="overlay"></div>
+            {modal10 &&(<div className="openModal">
+            <h3>{weather10.current.temp_c}°C</h3>
+            <img src={weather10.current.condition.icon} alt="" />
+            <h4>Szél erősség: {weather10.current.wind_kph} km/h</h4>
+            <h4>Hőérzet:{weather10.current.feelslike_c} °C</h4>
+            <h4>Páratartalom: {weather10.current.humidity} %</h4>
+            <button className="closeModal" onClick={toggleModal10}>Bezár</button>
+            </div>)}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
