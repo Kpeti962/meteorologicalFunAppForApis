47c4e2e6740993fc90a118c6ec84db4b8044113d
Peti <kpeti962@gmailcom>
2022-08-26 15:03:35 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import hungary from "./img/hungary.png";
 import "./styles/app.scss";
 import axios from "axios";
+import {motion} from "framer-motion";
+import {modalAnim} from "./animations"
 
 function App() {
   const [weather, setWeather] = useState("");
@@ -184,163 +186,165 @@ function App() {
           <div className="szeged">
             <h3 onClick={toggleModal}>{weather.location.name}</h3>
             <div onClick={toggleModal} className="overlay"></div>
-            {modal &&(<div className="openModal">
+            {modal &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3 onClick={toggleModal}>{weather.location.name}</h3>
-              <h3>{weather.current.temp_c}°C</h3>
+              <h3>{weather.current.temp_c} °C</h3>
               <img src={weather.current.condition.icon} alt="" />
               <h4>Szél erősség: {weather.current.wind_kph} km/h</h4>
               <h4>Hőérzet:{weather.current.feelslike_c} °C</h4>
               <h4>Páratartalom: {weather.current.humidity} %</h4>
 
               <button className="closeModal" onClick={toggleModal}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather1 && (
           <div className="pecs">
             <h3  onClick={toggleModal1}>{weather1.location.name}</h3>
             <div onClick={toggleModal1} className="overlay"></div>
-            {modal1 &&(<div className="openModal">
+            {modal1 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3  onClick={toggleModal1}>{weather1.location.name}</h3>
-            <h3>{weather1.current.temp_c}°C</h3>
+            <h3>{weather1.current.temp_c} °C</h3>
             <img src={weather1.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather1.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather1.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather1.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal1}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather2 && (
           <div className="keszthely">
             <h3   onClick={toggleModal2}>{weather2.location.name}</h3>
             <div onClick={toggleModal2} className="overlay"></div>
-            {modal2 &&(<div className="openModal">
+            {modal2 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3   onClick={toggleModal2}>{weather2.location.name}</h3>
-            <h3>{weather2.current.temp_c}°C</h3>
+            <h3>{weather2.current.temp_c} °C</h3>
             <img src={weather2.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather2.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather2.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather2.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal2}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather3 && (
           <div className="szombathely">
             <h3 onClick={toggleModal3}>{weather3.location.name}</h3>
             <div onClick={toggleModal3} className="overlay"></div>
-            {modal3 &&(<div className="openModal">
+            {modal3 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3   onClick={toggleModal3}>{weather3.location.name}</h3>
-            <h3>{weather3.current.temp_c}°C</h3>
+            <h3>{weather3.current.temp_c} °C</h3>
             <img src={weather3.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather3.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather3.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather3.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal3}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather4 && (
           <div className="sopron">
             <h3 onClick={toggleModal4}>{weather4.location.name}</h3>
             <div onClick={toggleModal1} className="overlay"></div>
-            {modal4 &&(<div className="openModal">
+            {modal4 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3 onClick={toggleModal4}>{weather4.location.name}</h3>
-            <h3>{weather4.current.temp_c}°C</h3>
+            <h3>{weather4.current.temp_c} °C</h3>
             <img src={weather4.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather4.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather4.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather4.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal4}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather5 && (
           <div className="gyor">
             <h3 onClick={toggleModal5}>{weather5.location.name}</h3>
             <div onClick={toggleModal5} className="overlay"></div>
-            {modal5 &&(<div className="openModal">
+            {modal5 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3 onClick={toggleModal5}>{weather5.location.name}</h3>
-            <h3>{weather5.current.temp_c}°C</h3>
+            <h3>{weather5.current.temp_c} °C</h3>
             <img src={weather5.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather5.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather5.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather5.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal5}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather6 && (
           <div className="szekesfehervar">
             <h3 onClick={toggleModal6}>{weather6.location.name}</h3>
             <div onClick={toggleModal6} className="overlay"></div>
-            {modal6 &&(<div className="openModal">
+            {modal6 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3 onClick={toggleModal6}>{weather6.location.name}</h3>
-            <h3>{weather6.current.temp_c}°C</h3>
+            <h3>{weather6.current.temp_c} °C</h3>
             <img src={weather6.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather6.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather6.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather6.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal6}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather7 && (
           <div className="budapest">
             <h3 onClick={toggleModal7}>{weather7.location.name}</h3>
             <div onClick={toggleModal7} className="overlay"></div>
-            {modal7 &&(<div className="openModal">
+            {modal7 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
             <h3 onClick={toggleModal7}>{weather7.location.name}</h3>
-            <h3>{weather7.current.temp_c}°C</h3>
+            <h3>{weather7.current.temp_c} °C</h3>
             <img src={weather7.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather7.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather7.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather7.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal7}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather8 && (
           <div className="szolnok">
             <h3 onClick={toggleModal8}>{weather8.location.name}</h3>
             <div onClick={toggleModal8} className="overlay"></div>
-            {modal8 &&(<div className="openModal">
-            <h3>{weather8.current.temp_c}°C</h3>
+            {modal8 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
+            <h3>{weather8.current.temp_c} °C</h3>
             <img src={weather8.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather8.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather8.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather8.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal8}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather9 && (
           <div className="miskolc">
             <h3 onClick={toggleModal9}>{weather9.location.name}</h3>
             <div onClick={toggleModal9} className="overlay"></div>
-            {modal9 &&(<div className="openModal">
-            <h3>{weather9.current.temp_c}°C</h3>
+            {modal9 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
+            <h3 onClick={toggleModal9}>{weather9.location.name}</h3>
+            <h3>{weather9.current.temp_c} °C</h3>
             <img src={weather9.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather9.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather9.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather9.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal9}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
         {weather10 && (
           <div className="debrecen">
             <h3 onClick={toggleModal10}>{weather10.location.name}</h3>
             <div onClick={toggleModal10} className="overlay"></div>
-            {modal10 &&(<div className="openModal">
-            <h3>{weather10.current.temp_c}°C</h3>
+            {modal10 &&(<motion.div variants={modalAnim} initial="hidden" animate="show" exit="exit" className="openModal">
+            <h3 onClick={toggleModal10}>{weather10.location.name}</h3>
+            <h3>{weather10.current.temp_c} °C</h3>
             <img src={weather10.current.condition.icon} alt="" />
             <h4>Szél erősség: {weather10.current.wind_kph} km/h</h4>
             <h4>Hőérzet:{weather10.current.feelslike_c} °C</h4>
             <h4>Páratartalom: {weather10.current.humidity} %</h4>
             <button className="closeModal" onClick={toggleModal10}>Bezár</button>
-            </div>)}
+            </motion.div>)}
           </div>
         )}
       </div>
---
+++ src/animations.js
@@ -0,0 +1,17 @@
+export const modalAnim = {
+        hidden: {
+            y: -300,
+            opacity: 0,
+        },
+        show: {
+            y: 0,
+            opacity: 0.9,
+            transition:{duration: 0.7,}
+        },
+        exit: {
+            y: -300,
+            opacity: 0,
+            transition:{duration: 0.7,}
+        },
+   
+}
